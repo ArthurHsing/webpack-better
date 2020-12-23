453af5e5329ed69c21afcf22a912e74c8c36e822
幸成 <573105494@qq.com>
2020-12-23 20:43:34 +0800

    realize a babel loader by myself

--- 03.loader/src/index.js
+++ 03.loader/src/index.js
@@ -1,1 +1,12 @@
 console.log('hello arthur');
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  setName(name) {
+    this.name = name;
+  }
+}
+
+console.log(new Person('arthur'));
--- 03.loader/webpack.config.js
+++ 03.loader/webpack.config.js
@@ -11,17 +11,23 @@ module.exports = {
         // loader: 'loader1' //在resolveLoader里面指定了寻找的路径就不用再在这里具体指定路径了
         // 多个loader用use，是一个数组，且loader从后往前执行
         // 如果loader里面有pitch方法，那么pitch方法会根据loader的顺序从前往后执行一遍
-        use: [
-          'loader1',
-          'loader2',
-          {
-            loader: 'loader3',
-            options: {
-              name: 'jack',
-              age: 18
-            }
-          }
-        ]
+        // use: [
+        //   'loader1',
+        //   'loader2',
+        //   {
+        //     loader: 'loader3',
+        //     options: {
+        //       name: 'jack',
+        //       age: 18
+        //     }
+        //   }
+        // ]
+        loader: 'babelLoader',
+        options: {
+          presets: [
+            '@babel/preset-env'
+          ]
+        }
       }
     ]
   },
@@ -34,5 +40,6 @@ module.exports = {
       // 指定自己的
       path.resolve(__dirname, 'loaders')
     ]
-  }
+  },
+  mode: 'production'
 }
